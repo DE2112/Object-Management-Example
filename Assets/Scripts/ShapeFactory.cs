961a1f95f8cffeee1b51781ac4b8ca50b84834c8
DE2112 <DE2112@bitbucket.org>
2022-04-12 00:25:26 +0300

    object pools added & shape recycling implemented

--- Assets/Scripts/ShapeFactory.cs
+++ Assets/Scripts/ShapeFactory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ObjectManagementExample
@@ -6,12 +7,40 @@ namespace ObjectManagementExample
     public class ShapeFactory : ScriptableObject
     {
         [SerializeField] private Shape[] _prefabs;
-        [SerializeField] private Material[] _materials; 
+        [SerializeField] private Material[] _materials;
+        [SerializeField] private bool _recycle;
+        private List<Shape>[] _pools;
 
         public Shape GetShape(int shapeId, int materialId)
         {
-            var instance = Instantiate(_prefabs[shapeId]);
-            instance.ShapeId = shapeId;
+            Shape instance;
+            if (_recycle)
+            {
+                if (_pools == null)
+                {
+                    CreatePools();
+                }
+                
+                var pool = _pools[shapeId];
+                var lastIndex = pool.Count - 1;
+                if (lastIndex >= 0)
+                {
+                    instance = pool[lastIndex];
+                    instance.gameObject.SetActive(true);
+                    pool.RemoveAt(lastIndex);
+                }
+                else
+                {
+                    instance = Instantiate(_prefabs[shapeId]);
+                    instance.ShapeId = shapeId;
+                }
+            }
+            else
+            {
+                instance = Instantiate(_prefabs[shapeId]);
+                instance.ShapeId = shapeId;
+            }
+            
             instance.SetMaterial(_materials[materialId], materialId);
             return instance;
         }
@@ -19,6 +48,32 @@ namespace ObjectManagementExample
         public Shape GetRandomShape()
         {
             return GetShape(Random.Range(0, _prefabs.Length), Random.Range(0, _materials.Length));
+        }
+
+        private void CreatePools()
+        {
+            _pools = new List<Shape>[_prefabs.Length];
+            for (int i = 0; i < _pools.Length; i++)
+            {
+                _pools[i] = new List<Shape>();
+            }
+        }
+
+        public void ReclaimShape(Shape shape)
+        {
+            if (_recycle)
+            {
+                if (_pools == null)
+                {
+                    CreatePools();
+                }
+                _pools[shape.ShapeId].Add(shape);
+                shape.gameObject.SetActive(false);
+            }
+            else
+            {
+                Destroy(shape.gameObject);
+            }
         }
     }
 }
