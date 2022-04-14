e86387627efb74fb3d0d6511c3114f388c8d7e4f
DE2112 <DE2112@bitbucket.org>
2022-04-14 18:01:25 +0300

    level data saving/loading implemented

--- Assets/Scripts/GameLevel.cs
+++ Assets/Scripts/GameLevel.cs
@@ -1,14 +1,29 @@
+using System;
 using UnityEngine;
 
 namespace ObjectManagementExample
 {
-    public class GameLevel : MonoBehaviour
+    public class GameLevel : PersistableObject
     {
+        private static GameLevel _current;
         [SerializeField] private SpawnZone _spawnZone;
-        
-        private void Start()
+
+        public static GameLevel Current => _current;
+        public Vector3 SpawnPoint => _spawnZone.GetSpawnPoint();
+
+        private void OnEnable()
         {
-            Game.Instance.SpawnZoneOfLevel = _spawnZone;
+            _current = this;
+        }
+
+        public override void Save(GameDataWriter writer)
+        {
+            base.Save(writer);
+        }
+
+        public override void Load(GameDataReader reader)
+        {
+            base.Load(reader);
         }
     }
 }
